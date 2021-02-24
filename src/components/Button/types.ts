445568a9127a600ba09a9e2b5e6978f51646e5b7
alex08g <allex.araujo088@gmail.com>
2021-02-24 10:09:32 -0300

    feat: adicionando tipo disable ao componente Button

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,6 +1,6 @@
 import React, { ButtonHTMLAttributes } from 'react'
 
-export type ButtonColorType = | 'primary' | 'danger'
+export type ButtonColorType = | 'primary' | 'danger' | 'disable'
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   color: ButtonColorType
