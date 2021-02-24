6911fbe6521c8783878f7468eb3a39f6ef9a3abf
alex08g <allex.araujo088@gmail.com>
2021-02-24 07:53:14 -0300

    feat: criando componente countdown

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,18 +1,46 @@
+import { useEffect, useState } from 'react'
 import * as Styles from './styles'
 
 const Countdown: React.FC = () => {
+  const [time, setTime] = useState(25 * 60)
+  const [active, setActive] = useState(false)
+
+  const minutes = Math.floor(time / 60)
+  const seconds = time % 60
+
+  const [minutesLeft, minutesRight] = String(minutes).padStart(2, '0').split('')
+  const [secondsLeft, secondsRight] = String(seconds).padStart(2, '0').split('')
+
+  const startCountdown = (): void => {
+    setActive(true)
+  }
+
+  useEffect(() => {
+    if(active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1)
+      }, 1000)
+    }
+  }, [active, time])
+
   return (
-    <Styles.Container>
-      <Styles.Minutes>
-        <span>2</span>
-        <span>5</span>
-      </Styles.Minutes>
-      <span>:</span>
-      <Styles.Seconds>
-        <span>0</span>
-        <span>0</span>
-      </Styles.Seconds>
-    </Styles.Container>
+    <Styles.ContainerCountdown>
+      <Styles.Countdown>
+        <div>
+          <span>{minutesLeft}</span>
+          <span>{minutesRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondsLeft}</span>
+          <span>{secondsRight}</span>
+        </div>
+      </Styles.Countdown>
+
+      <Styles.Button type="button" onClick={startCountdown}>
+        Iniciar um ciclo
+      </Styles.Button>
+    </Styles.ContainerCountdown>
   )
 }
 
--- src/components/Countdown/styles.tsx
+++ src/components/Countdown/styles.tsx
@@ -1,18 +1,71 @@
 import styled from "styled-components"
 
-export const Container  = styled.div`
+export const ContainerCountdown  = styled.div``
+
+export const Countdown  = styled.div`
   display: flex;
   align-items: center;
 
   font-family: Rajdhani;
   font-weight: 600;
   color: var(--title);
+
+  & > div {
+    flex: 1;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+
+    background-color: var(--white);
+    box-shadow: 0 0 60px rgba(0, 0, 0, 0.05);
+
+    border-radius: 0.5rem;
+
+    font-size: 13.6rem;
+    text-align: center;
+  }
+
+  & > div span {
+    flex: 1;
+  }
+
+  & > div span:first-child {
+    border-right: 0.1rem solid var(--background);
+  }
+
+  & > div span:last-child {
+    border-left: 0.1rem solid var(--background);
+  }
+
+  & > span {
+    font-size: 10rem;
+
+    margin: 0 0.8rem;
+  }
 `
 
-export const Minutes  = styled.div`
+export const Button  = styled.button`
+  width: 100%;
+  height: 8rem;
+  margin-top: 3.2rem;
 
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-export const Seconds  = styled.div`
+  border: none;
+  border-radius: 0.5rem;
 
+  background-color: var(--blue);
+  color: var(--white);
+
+  font-size: 2rem;
+  font-weight: 600;
+
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    background-color: var(--blue-dark);
+  }
 `
