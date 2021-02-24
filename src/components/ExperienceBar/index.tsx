551ab00ca69985c9edcd2dee337ea1682c597404
alex08g <allex.araujo088@gmail.com>
2021-02-24 15:01:36 -0300

    feat: utilizando contexto

--- src/components/ExperienceBar/index.tsx
+++ src/components/ExperienceBar/index.tsx
@@ -1,15 +1,21 @@
+import useChallenge from '../../hooks/useChallenge'
 import * as Styles from './styles'
 
-const ExperienceBar: React.FC  = () => {
+const ExperienceBar: React.FC = () => {
+  const { currentExperience, experienceToNextLevel } = useChallenge()
+
+  const percentLevel =
+    Math.round(currentExperience * 100) / experienceToNextLevel
+
   return (
-    <Styles.Container>
+    <Styles.Container percentLevel={percentLevel}>
       <span>0 XP</span>
       <div>
         <div />
 
-        <span className="current-experience" >300 XP</span>
+        <span className="current-experience">{currentExperience} XP</span>
       </div>
-      <span>600 XP</span>
+      <span>{experienceToNextLevel} XP</span>
     </Styles.Container>
   )
 }
