fecd77aa93a0ad15ed580e1c249cab0761f46e72
alex08g <allex.araujo088@gmail.com>
2021-02-25 09:54:51 -0300

    feat: provendo o contexto countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Head from 'next/head'
 
-import CountdownProvider from '../contexts/CountdownContext/indext'
-import CompletedChallenges from '../components/CompletedChallenges'
-import ExperienceBar from '../components/ExperienceBar'
+import Head from 'next/head'
 import ChallengeBox from '../components/ChallengeBox'
+
+import CompletedChallenges from '../components/CompletedChallenges'
 import Countdown from '../components/Countdown'
+import ExperienceBar from '../components/ExperienceBar'
 import Profile from '../components/Profile'
 
 import * as Styles from '../styles/pages/Home'
@@ -17,18 +17,16 @@ const Home: React.FC = () => {
         <title>Inicio | Moveit</title>
       </Head>
       <ExperienceBar />
-      <CountdownProvider>
-        <Styles.Wrapper>
-          <Styles.LetfContainer>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </Styles.LetfContainer>
-          <Styles.RightContainer>
-            <ChallengeBox />
-          </Styles.RightContainer>
-        </Styles.Wrapper>
-      </CountdownProvider>
+      <Styles.Wrapper>
+        <Styles.LetfContainer>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </Styles.LetfContainer>
+        <Styles.RightContainer>
+          <ChallengeBox />
+        </Styles.RightContainer>
+      </Styles.Wrapper>
     </Styles.Container>
   )
 }
