71b14a0700c4e74433f60543e9f91f4cb1999ea0
alex08g <allex.araujo088@gmail.com>
2021-02-25 09:50:10 -0300

    feat: criando contexto pro countdown

---
+++ src/contexts/CountdownContext/indext.tsx
@@ -0,0 +1,60 @@
+import React, { useState, createContext, useEffect } from 'react'
+import useChallenge from '../../hooks/useChallenge'
+import { CountdownContextProps, CountdownProviderProps } from './types'
+
+export const CountdownContext = createContext({} as CountdownContextProps)
+
+let countdownTimeout: NodeJS.Timeout
+
+const CountdownProvider = ({
+  children
+}: CountdownProviderProps): JSX.Element => {
+  const { startNewChallenge } = useChallenge()
+
+  const [time, setTime] = useState(0.1 * 60)
+  const [isActive, setIsActive] = useState(false)
+  const [hasFinished, setHasFinished] = useState(false)
+
+  const minutes = Math.floor(time / 60)
+  const seconds = time % 60
+
+  const startCountdown = (): void => {
+    setIsActive(true)
+  }
+
+  const resetCountdown = (): void => {
+    clearTimeout(countdownTimeout)
+    setIsActive(false)
+    setHasFinished(false)
+    setTime(0.1 * 60)
+  }
+
+  useEffect(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1)
+      }, 1000)
+    } else if (isActive && time === 0) {
+      setHasFinished(true)
+      setIsActive(false)
+      startNewChallenge()
+    }
+  }, [isActive, time])
+
+  return (
+    <CountdownContext.Provider
+      value={{
+        minutes,
+        seconds,
+        hasFinished,
+        isActive,
+        startCountdown,
+        resetCountdown
+      }}
+    >
+      {children}
+    </CountdownContext.Provider>
+  )
+}
+
+export default CountdownProvider
---
+++ src/contexts/CountdownContext/types.ts
@@ -0,0 +1,14 @@
+import { ReactNode } from 'react'
+
+export interface CountdownProviderProps {
+  children: ReactNode
+}
+
+export interface CountdownContextProps {
+  minutes: number
+  seconds: number
+  hasFinished: boolean
+  isActive: boolean
+  startCountdown: () => void
+  resetCountdown: () => void
+}
